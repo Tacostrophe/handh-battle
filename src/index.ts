e3aa945409c24b8f0adff400b69891876a88bb15
tacostrophe <tacokong@yandex.ru>
2023-10-03 09:31:47 +0300

    heal added

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,20 @@ import { Monster } from './monster.class';
 import { Player } from './player.class';
 
 const agressor = new Player('Player', 25, 1, 200, {min: 100, max: 200});
-const victim = new Monster('Monster', 20, 23, 1000);
+const victim = new Monster('Monster', 20, 23, 1000, {min: 500, max: 1000});
 
 console.log(agressor.toString());
 console.log(victim.toString());
+
+agressor.heal();
 
 for(let i = 0; i < 5; i++) {
   agressor.hit(victim);
 }
 
 victim.hit(agressor);
+
+agressor.heal();
 
 console.log(agressor.toString());
 console.log(victim.toString());
